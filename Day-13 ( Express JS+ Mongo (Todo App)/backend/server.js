eb233e4b934664307334c5ea17e66cb835eee9b6
Himanshu Bhardwaj <bhardwajhimanshu2515@gmail.com>
2021-05-03 17:08:30 +0530

    himanshu || signup/login api created

--- Day-13 ( Express JS+ Mongo (Todo App)/backend/server.js
+++ Day-13 ( Express JS+ Mongo (Todo App)/backend/server.js
@@ -31,6 +31,67 @@ mongodb.MongoClient.connect(
       return console.log(err);
     }
     console.log("connected to mongoDb");
+    const newDb = client.db("todoDataBaseProject");
+    //api create here
+
+    //signup api create
+    app.post("/signup", (req, res) => {
+      console.log(req.body);
+      const { name, phoneNumber, email, password } = req.body;
+
+      //check if user already exist or not
+      let existingUser = newDb
+        .collection("user")
+        .find({ email: email })
+        .toArray();
+      existingUser.then((User) => {
+        //if user already exist
+        if (User.length > 0) {
+          return res.status(201).json("User Already Exist");
+        }
+        //if user does not exist 
+        else if (User.length < 1) {
+          let createUser = newDb
+            .collection("user")
+            .insertOne({
+              name: name,
+              phoneNumber: phoneNumber,
+              email: email,
+              password: password,
+            });
+          createUser.then((User) => {
+            res.status(200).json(User.ops);
+          });
+        }
+      });
+    });
+
+    //login api
+    app.post("/login",(req,res)=>{
+      console.log(req.body);
+      const {email,password}=req.body;
+
+      //check user exist or not
+      let existingUser=newDb.collection('user').find({email:email}).toArray();
+      existingUser.then((User)=>{
+        //if user not exist
+        if(User.length<1){
+          return res.status(201).json("User does not exist, please signup Instead ")
+        }
+        //if user exist
+        else if(User.length>0){
+          //if pass match
+          if(password===User[0].password){
+            return res.status(200).json(User)
+          }
+          //if pass not match
+          else{
+            res.status(201).json("Password Didn't match");
+          }
+        }
+      })
+
+    })
   }
 );
 //run server
